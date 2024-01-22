6fc91b6a34fa1045c77e4d6a8b70509fe19b70c7
MyNameTri <crisdien98@gmail.com>
2024-01-22 23:46:21 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default App
