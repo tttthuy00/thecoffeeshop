2acc156e17079180c29000428079a05944495675
tailcps22368 <chitaiteo@gmail.com>
2024-01-28 12:25:12 +0700

    welcome,authorization, forgotpassword, registration, twofactorverification, starupscreen

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Welcome from './src/component/app/news/screen/Welcome'
+import Authorization from './src/component/app/news/screen/Authorization';
+import Forgot_password from './src/component/app/news/screen/Forgot_password';
+import Registration from './src/component/app/news/screen/Registration';
+import Two_factor_verification from './src/component/app/news/screen/Two_factor_verification';
+import Startup_screen from './src/component/app/news/screen/Startup_screen';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <Welcome/>
+    // <Authorization/>
+    // <Registration/>
+    // <Forgot_password/>
+    // <Two_factor_verification/>
+    // <Startup_screen/>
   )
 }
 
-export default App
+export default App;
